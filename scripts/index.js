d4e37f4d94f422fce0b82d4d00a9e11689aa41a0
mkhalil1997 <mkhalil1997@outlook.com>
2022-09-16 22:43:50 +0300

    commented JS code

--- scripts/index.js
+++ scripts/index.js
@@ -12,6 +12,7 @@ function eventlist(){
     document.querySelectorAll(".posdiv").forEach((item,i)=>{
         item.addEventListener('click', function(){
             items[i] = item;
+            //condition to know which player's turn it is
             if(turn%2 == 0){
                 item.style.backgroundImage = "url(assets/red.png)";
                 red.push(`${i+1}`);
@@ -22,6 +23,7 @@ function eventlist(){
                 yellow.push(`${i+1}`);
                 checkwin(yellow);
             }
+            item.style.cursor = "auto";
             this.removeEventListener('click', arguments.callee);
         })
     })
@@ -30,8 +32,11 @@ function eventlist(){
 
 //WIN CONDITIONS
 function checkwin(array){
-    console.log(items)
     if(
+        /*
+        Checking if any coniditon is met for win, I've got the idea to get the difference, I've noticed there are differences
+        of 1&3&4 accross all true conditions
+        */
         (array.includes("1") && array.includes("2") && array.includes("3")) ||
         (array.includes("4") && array.includes("5") && array.includes("6")) ||
         (array.includes("7") && array.includes("8") && array.includes("9")) ||
@@ -41,20 +46,21 @@ function checkwin(array){
         (array.includes("2") && array.includes("5") && array.includes("8")) ||
         (array.includes("3") && array.includes("6") && array.includes("9"))
     ){
-        console.log("won")
+        //if yellow is the player who won
         if(yellow == array){
             document.getElementById("mid").innerText = "Player 2 WON!"
             player2++;
         }
+        //if red is the player who won
         else{
             player1++;
             document.getElementById("mid").innerText = "Player 1 WON!"
             document
         }
-        yellow = [];
-        red = [];
+        //reset in any case
         reset();
     }
+    //If we have filled all 9 spots reset
     else if((yellow.length + red.length) == 9){
         reset()
     }
@@ -62,6 +68,7 @@ function checkwin(array){
 }
 
 function reset(){
+    //Styling for winner, loser and in case of draw
     if(player1>player2){
         document.getElementById("score1").style.color = "green";
         document.getElementById("score2").style.color = "red";
@@ -74,17 +81,21 @@ function reset(){
         document.getElementById("score1").style.color = "yellow";
         document.getElementById("score2").style.color = "yellow";
     }
+    //reset all array and turn, i've considered alternating turn, however, didn't have the time to do it
     yellow = [];
     red = [];
     turn = 0;
+    //update score
     document.getElementById("score1").innerText = player1;
     document.getElementById("score2").innerText = player2;
+    //Setting a small timeout before readding the event listners, also reset the background images and the cursor to pointer
     setTimeout(()=>{
     items.forEach((item,i)=>{
             if(!item){
                 return;
             }
             item.style.backgroundImage = "";
+            item.style.cursor = "pointer";
             item.addEventListener('click', function(){
                 items[i] = item;
                 if(turn%2 == 0){
@@ -97,6 +108,7 @@ function reset(){
                     yellow.push(`${i+1}`);
                     checkwin(yellow);
                 }
+                item.style.cursor = "auto";
                 this.removeEventListener('click', arguments.callee);
             })
         })
